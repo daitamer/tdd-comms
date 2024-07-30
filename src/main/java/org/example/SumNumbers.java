64b654e4803e3e907eb2d9034851145c3e557ab7
Despina Ait-Amer <despinaaitamer@Despinas-MacBook-Air.local>
2024-07-30 11:34:26 +0100

    test: first test

---
+++ src/main/java/org/example/SumNumbers.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class SumNumbers {
+
+    public int sumNumbers(int[] input){
+        return 0;
+    }
+
+}
---
+++ src/test/java/org/example/SumNumbersTest.java
@@ -0,0 +1,39 @@
+package org.example;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+class SumNumbersTest {
+
+    SumNumbers sumNumbers;
+    int[] input;
+    int[] input2;
+    int[] input3;
+
+    @BeforeEach
+    void setUp() {
+        sumNumbers = new SumNumbers();
+    }
+
+    @Test
+    @DisplayName("return 0 if int array is less than three digits long")
+    void sumNumbers() {
+
+        input = new int[]{1, 2};
+        input2 = new int[]{1};
+        input3 = new int[]{};
+
+        int result = sumNumbers.sumNumbers(input);
+        int result2 = sumNumbers.sumNumbers(input2);
+        int result3 = sumNumbers.sumNumbers(input3);
+
+        assertEquals(0, result);
+        assertEquals(0, result2);
+        assertEquals(0, result3);
+
+    }
+}
