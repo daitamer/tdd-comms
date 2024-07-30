109252ce47ba0e790fb69a73993562de4eecb035
Joel <joel@MacBook-Pro-Alex.local>
2024-07-30 15:56:29 +0100

    test: solve testsumAsciiValue with special chars

--- src/main/java/org/example/AsciiValues.java
+++ src/main/java/org/example/AsciiValues.java
@@ -1,9 +1,23 @@
 package org.example;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class AsciiValues {
     public static int sumAsciiValue(String input) {
         int sum = 0;
+        String regex = "^[a-zA-Z0-9]+$";
         String trimmedInput = input.replace(" ", "");
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(trimmedInput);
+
+        if (!matcher.matches()) {
+//            throw new IllegalArgumentException("Invalid Input!!");
+            System.out.println(input);
+            return -1;
+        }
+
 
         for (int i = 0; i < trimmedInput.length(); i++) {
 
--- src/test/java/org/example/AsciiValuesTest.java
+++ src/test/java/org/example/AsciiValuesTest.java
@@ -10,6 +10,7 @@ class AsciiValuesTest {
 //    AsciiValues asciiValues;
     String input;
     String input2;
+    String input3;
     @BeforeEach
     void setUp() {
 //        asciiValues = new AsciiValues();
@@ -21,13 +22,16 @@ class AsciiValuesTest {
     //Arrange
         input = "Hello World";
         input2 = "Number 123";
+        input3 = "Number@#$";
     //Act
         int result = AsciiValues.sumAsciiValue(input);
         int result2 = AsciiValues.sumAsciiValue(input2);
+        int result3 = AsciiValues.sumAsciiValue(input3);
     //Assert
         assertAll("return sum when given a string of letters and digits",
                 () -> assertEquals(1020, result),
-                () -> assertEquals(767, result2)
+                () -> assertEquals(767, result2),
+                () -> assertEquals(-1, result3)
         );
     }
 }
